aa98f3a82c048be1a6216ac13c50e00e0b435d6f
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2022-11-24 21:22:59 +0530

    Time: 5 ms (78.63%), Space: 9.6 MB (51.64%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,24 +1,54 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int start = 0;
-        int row = (int)matrix.size();
-        int col = (int)matrix[0].size();
-        int end = row * col - 1;
+    bool binary(vector<vector<int>>& matrix, int a, int target)
+    {
+        int start = 0; 
+        int end = (int)matrix[0].size()-1;
         int mid;
-        while(start <= end){
+        while(start <= end)
+        {
             mid = (start + end) / 2;
-            int number = matrix[mid / col][mid % col];
-            if(target < number)
+            if(target < matrix[a][mid])
             {
                 end = mid - 1;
             }
-            else if(target > number)
+            else if(target > matrix[a][mid])
             {
                 start = mid + 1;
             }
-            else{
+            else if(target == matrix[a][mid])
+            {
                 return true;
+            }
+        }
+        return false;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int start = 0;
+        int n = (int)matrix[0].size() - 1;
+        int end = (int)matrix.size() - 1;
+        int mid;
+        while(start <= end){
+            mid = (start + end) / 2;
+            if(target == matrix[mid][0])
+            {
+                return true;
+            }
+            if(target == matrix[mid][n])
+            {
+                return true;
+            }
+            if(target < matrix[mid][0])
+            {
+                end = mid - 1;
+            }
+            else if( target > matrix[mid][n])
+            {
+                start = mid + 1;
+            }
+            else if(target > matrix[mid][0] && target < matrix[mid][n])
+            {
+                return binary(matrix, mid, target);
             }
         }
         return false;
