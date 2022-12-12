c4298c6681e173551689961bc490a8fb20b810c2
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2022-12-12 10:31:17 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (55.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,16 @@
 class Solution {
     
 public:
-    int a[46];
-    void ans()
-    {
-        a[1] = 1;
+    
+    
+    int climbStairs(int n) {
+        int a[46];
+         a[1] = 1;
         a[2] = 2;
         for(int i = 3; i < 46; ++i)
         {
             a[i] = a[i - 1] + a[i - 2];
         }
-    }
-    
-    int climbStairs(int n) {
-        ans();
        return a[n];
     }
 };
