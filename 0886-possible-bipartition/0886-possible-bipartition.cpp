d89d1d5fda08239ad2f7199a3a7db7b186bb13bb
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2022-12-21 23:12:24 +0530

    Time: 235 ms (85.48%), Space: 69.5 MB (43.42%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -0,0 +1,57 @@
+#define WHITE 0
+#define RED 1
+#define BLUE 2
+class Solution {
+public:
+    bool possibleBipartition(int n, vector<vector<int>>& edges) {
+        int len = (int)edges.size();
+        unordered_map<int, vector<int> > mp;
+        for(int i = 0 ; i < len; ++i)
+        {
+            mp[edges[i][0]].push_back(edges[i][1]);
+            mp[edges[i][1]].push_back(edges[i][0]);
+        }
+        vector<string> color(n + 1, "white");
+        queue<int> q;
+        for(auto i: mp)
+        {
+            while(!q.empty()) { q.pop(); }
+            int curr = i.first;
+            if(color[curr] == "white")
+            {
+                color[curr] = "blue";
+                q.push(curr);
+                while(!q.empty())
+                {
+                    int parent = q.front();
+                    q.pop();
+                    for(int i = 0; i < (int)mp[parent].size(); ++i)
+                    {
+                        int child = mp[parent][i];
+                        if(color[child] == "white")
+                        {
+                            if(color[parent] == "blue")
+                            {
+                                color[child] = "red";
+                            }
+                            else
+                            {
+                                color[child] = "blue";
+                            }
+                            q.push(child);
+                        }
+                        else
+                        {
+                            if(color[parent] == color[child])
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+            
+        }
+        return true;
+    }
+};
