3ba6690f289a664d03266bce5ea605aac9d30133
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2023-09-02 01:20:22 +0530

    Time: 3 ms (89.66%), Space: 8.5 MB (32.3%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countSetBits( int n)
+    {
+        int count = 0;
+        while (n) {
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+    }
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int i = 0; i <=n; ++i){
+            ans.push_back(countSetBits(i));
+        }
+        return ans;
+    }
+};
