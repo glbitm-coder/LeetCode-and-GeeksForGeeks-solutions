d9cdefaf015b6f570ef06e4e388b7ad7e46d037a
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2024-06-03 01:51:22 +0530

    Time: 6 ms (46.25%), Space: 16.5 MB (17.07%) - LeetHub

---
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<bool>> visited(m, vector<bool> (n, false));
+        queue<pair<int,int>> q;
+        for(int i = 0; i < m; ++i)
+        {
+            for(int j = 0; j < n; ++j){
+                
+                visited[i][j] = false;
+
+                if(grid[i][j] == 2){
+                    q.push({i, j});
+                }
+            }
+        }
+
+        vector<int> x{-1, 0, 1, 0};
+        vector<int> y{0, 1, 0, -1};
+
+        
+        int ans = 0;
+        q.push({-1, -1});
+        while(!q.empty())
+        {
+            pair<int,int> curr = q.front();
+
+            if(curr.first == -1 && curr.second == -1)
+            {
+                q.pop();
+                ans += 1;
+
+                if(!q.empty()){
+                    q.push({-1, -1});
+                }
+                continue;
+            }
+            q.pop();
+            visited[curr.first][curr.second] = true;
+            cout<<curr.first<<" "<<curr.second<<"\n";
+            for(int i = 0; i < 4; ++i){
+                int var_x = curr.first + x[i];
+                int var_y = curr.second + y[i];
+                if(var_x >= 0 && var_x < m && var_y >= 0 && var_y < n){
+                    if(grid[var_x][var_y] == 1 && !visited[var_x][var_y]){
+                        grid[var_x][var_y] = 2;
+                        q.push({var_x, var_y});
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                if(grid[i][j] == 1){
+                    return -1;
+                }
+            }
+        }
+
+        return ans - 1;
+    }
+};
