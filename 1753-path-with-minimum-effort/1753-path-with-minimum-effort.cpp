4346188db9eb7719a6e8f0b2f9f297d740472c19
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2024-06-12 02:00:38 +0530

    Time: 290 ms (7.54%), Space: 52.8 MB (12.61%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,46 +1,55 @@
 class Solution {
-    vector<vector<int>> dirs = {
-                {-1,0},
-        {0,-1},         {0,1},
-                {1, 0}
-    };
     
 public:
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        using pipii = pair<int,pair<int,int>>;
-        priority_queue<pipii, vector<pipii>, greater<pipii>> pq;
-        
-        int ROWS = heights.size();
-        int COLS = heights[0].size();
-        
-        vector<vector<int>> dists(ROWS, vector<int>(COLS, INT_MAX));
-        
-        dists[0][0] = 0;
-        
-        pq.push({0, {0,0}});
-        while(!pq.empty()) {
-            auto node = pq.top();
-            pq.pop();
-            
-            int weight = node.first;
-            auto coords = node.second;
-            int r = coords.first;
-            int c = coords.second;
-            if(dists[r][c] < weight) continue;
-            if(r == ROWS - 1 && c == COLS - 1) return weight;
-            
-            for(auto& dir : dirs) {
-                int dr = r + dir[0];
-                int dc = c + dir[1];
-                if(dr < 0 || dr >= ROWS) continue;
-                if(dc < 0 || dc >= COLS) continue;
-                int newWeight = abs(heights[r][c] - heights[dr][dc]);
-                newWeight = max(newWeight, weight);
-                if(dists[dr][dc] <= newWeight) continue;
-                dists[dr][dc] = newWeight;
-                pq.push({newWeight, {dr, dc}});         
-            }  
-        }     
-        return 0;      
+    int minimumEffortPath(vector<vector<int>>& heights) {  
+        set<pair<int,pair<int,int>>> s;
+
+        int rows = heights.size();
+        int cols = heights[0].size();
+
+        map<pair<int,int>,int> ans;
+
+        s.insert({0, {0,0}});
+
+        for(int i = 0; i < rows; ++i){
+            for(int j = 0; j < cols; ++j){
+                ans[{i,j}] = INT_MAX;
+            }
+        }
+        ans[{0,0}] = 0;
+
+        vector<int> xDir{0,0,1,-1};
+        vector<int> yDir{1,-1,0,0};
+
+        while(!s.empty())
+        {
+            auto it = *(s.begin());
+            int value = it.first;
+            int xValue = it.second.first;
+            int yValue = it.second.second;
+
+             s.erase(it);
+
+             for(int i = 0; i < 4; ++i){
+                int upDatedX = xValue + xDir[i];
+                int upDatedY = yValue + yDir[i];
+
+                if(upDatedX >= 0 && upDatedY >= 0 && upDatedX < rows && upDatedY < cols)
+                {
+                    // cout<<xValue<<" "<<yValue<<" "<<upDatedX<<" "<<upDatedY<<"\n";
+                    int prevVal = heights[xValue][yValue];
+                    int currentVal = heights[upDatedX][upDatedY];
+                    
+                    if(ans[{upDatedX,upDatedY}] > max(value, abs(currentVal - prevVal))){
+                        // cout<<xValue<<" "<<yValue<<" "<<upDatedX<<" "<<upDatedY<<"\n";
+                        ans[{upDatedX,upDatedY}] = max(value, abs(currentVal - prevVal));
+                        s.insert({ans[{upDatedX,upDatedY}], {upDatedX,upDatedY}});
+                    }
+                    
+                }
+             }
+        }
+        // cout<<ans[{1,0}]<<"\n";
+        return ans[{rows - 1, cols - 1}];
     }
 };
