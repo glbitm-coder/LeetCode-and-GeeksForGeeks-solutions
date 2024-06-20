10a6d0ffeee66aacb2ac88251c820379ee665ae3
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2024-06-21 02:15:55 +0530

    Time: 0 ms (100%), Space: 10.2 MB (63.32%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -10,41 +10,27 @@ public:
         }
 
         for(int i = 1; i < m; ++i){
-            if(obstacleGrid[i][0] == 1 || dp[i - 1][0] == -1){
-                dp[i][0] = -1; 
+            if(obstacleGrid[i][0] == 1 || dp[i - 1][0] == 0){
+                dp[i][0] = 0; 
             }
         }
         for(int i = 1; i < n; ++i){
-            if(obstacleGrid[0][i] == 1 || dp[0][i - 1] == -1){
-                dp[0][i] = -1;
+            if(obstacleGrid[0][i] == 1 || dp[0][i - 1] == 0){
+                dp[0][i] = 0;
             }
         }
 
         for(int i = 1; i < m; ++i){
             for(int j = 1; j < n; ++j){
                 if(obstacleGrid[i][j] == 1){
-                    dp[i][j] = -1;
+                    dp[i][j] = 0;
                     continue;
                 }
 
-                int upper = dp[i - 1][j];
-                int left = dp[i][j - 1];
-                if(upper == -1 && left == -1){
-                    dp[i][j] = -1;
-                    continue;
-                }
-                if(upper == -1){
-                    dp[i][j] = left;
-                    continue;
-                }
-                if(left == -1){
-                    dp[i][j] = upper;
-                    continue;
-                }
-                dp[i][j] = left + upper;
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
 
-        return dp[m - 1][n - 1]  == -1 ? 0 : dp[m - 1][n - 1];
+        return dp[m - 1][n - 1];
     }
 };
