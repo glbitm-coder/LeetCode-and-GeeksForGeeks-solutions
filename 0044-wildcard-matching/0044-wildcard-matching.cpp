fb9d79351aaa3410b20ac5b29c586311135c64b6
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2024-07-04 02:09:44 +0530

    Time: 78 ms (13%), Space: 15.7 MB (64.02%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -37,8 +37,40 @@ public:
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        vector<vector<long long int>> dp(n + 1, vector<long long int> (m + 1, -1));
-        return check(0, s, 0, p, n, m, dp);
+        //vector<vector<long long int>> dp(n + 1, vector<long long int> (m + 1, -1));
+        // return check(0, s, 0, p, n, m, dp);
+        vector<vector<bool>> dp(n + 1, vector<bool> (m + 1, false));
+        dp[0][0] = true;
 
+        for(int i = 1; i <= n; ++i){
+            dp[i][0] = false;
+        }
+
+        for(int i = 1; i <= m; ++i){
+            bool flag = true;
+            for(int j = 1; j <= i; ++j){
+                if(p[j - 1] != '*'){
+                    flag = false;
+                    break;
+                }
+            }
+            dp[0][i] = flag;
+        }
+
+        for(int i = 1; i <= n; ++i){
+            for(int j = 1; j <= m; ++j){
+                if(s[i - 1] == p[j - 1] || p[j - 1] == '?'){
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else if(p[j - 1] == '*'){
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+                else{
+                    dp[i][j] = false;
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
