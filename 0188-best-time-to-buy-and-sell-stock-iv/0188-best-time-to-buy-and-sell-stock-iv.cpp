64414a60706f57048c0236477730f3156e084848
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2024-07-13 03:20:17 +0530

    Time: 6 ms (78.8%), Space: 14.7 MB (66.21%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -6,15 +6,12 @@ public:
         vector<vector<int>> dp(k + 1, vector<int> (n, 0));
 
         for(int i = 1; i <= k; ++i){
+            int val = INT_MIN;
             for(int j = 1; j < n; ++j){
-                for(int p = 0; p < j; ++p){
-                    dp[i][j] = max(dp[i][j], dp[i - 1][p] + prices[j] - prices[p]);
-                }
-                dp[i][j] = max(dp[i][j], dp[i][j - 1]);
-                dp[i][j] = max(dp[i - 1][j], dp[i][j]);
+                val = max(val, dp[i - 1][j - 1] - prices[j - 1]);
+                dp[i][j] = max(val + prices[j], dp[i][j - 1]);
             }
         }
-
         return dp[k][n - 1];
     }
 };
