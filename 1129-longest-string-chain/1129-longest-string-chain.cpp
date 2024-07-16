904b0531ce5f8edb739ec15e55a29b710e8e19cd
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2024-07-16 16:09:35 +0530

    Time: 1715 ms (16.72%), Space: 638.9 MB (10.91%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -1,19 +1,61 @@
 class Solution {
 public:
-    int longestStrChain(vector<string> &words) {
-        unordered_map<string, int> dp;
-        int res = 1;
-        sort(words.begin(), words.end(), [](const string &l, const string &r) { return l.size() < r.size(); });
-        for (string word : words) {
-            dp[word] = 1;
-            for (int i = 0; i < word.size(); i++) {
-                string prev = word.substr(0, i) + word.substr(i + 1);
-                if (dp.find(prev) != dp.end()) {
-                    dp[word] = max ( dp[word] , dp[prev] + 1 ) ;
-                    res = max(res, dp[word]);
-                }
+    bool isValid(string s1, string s2){
+        int n1 = s1.size();
+        int n2 = s2.size();
+
+        if(n1 - 1 != n2){
+            return false;
+        }
+
+        int count = 0;
+        for(int i = 0, j = 0; i < n1 && j < n2;){
+            if(s1[i] == s2[j]){
+                count++;
+                ++i;
+                ++j;
+            }
+            else{
+                ++i;
             }
         }
-        return res;
+
+        if(count == n2){
+            return true;
+        }
+        return false;
+    }
+
+    bool static cmp(string &a, string &b){
+
+        if(a.size() != b.size()){
+            return a.size() < b.size();
+        }
+        else{
+            return a < b;
+        }
+    }
+
+    int longestStrChain(vector<string>& words) {
+        
+        int n = words.size();
+        vector<int> dp(n, 0);
+
+        sort(words.begin(), words.end(), cmp);
+
+        dp[0] = 1;
+        int ans = 1;
+        for(int i = 1; i < n; ++i){
+            int maxi = 1;
+            for(int j = 0; j < i; ++j){
+                if(isValid(words[i], words[j])){
+                    maxi = max(maxi, dp[j] + 1);
+                }
+            }
+            dp[i] = maxi;
+            ans = max(ans, maxi);
+        }
+
+        return ans;
     }
 };
