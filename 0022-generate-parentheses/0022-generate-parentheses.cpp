210c2ca3fdf5b2aa59a38ab8ccdf3b0bac9a8a22
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2024-12-14 17:52:23 +0530

    Time: 5 ms (21.6%), Space: 10.8 MB (96.72%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    vector<string> helper(int n){
+        vector<string> ans;
+        unordered_set<string> s;
+        if(n == 1){
+            ans.push_back("()");
+            return ans;
+        }
+        for(auto it:helper(n-1)){
+            s.insert("()" + it);
+            for(int i = 0; i < it.size(); ++i){
+                s.insert(it.substr(0,i + 1) + "()" + it.substr(i+1));
+            }
+        }
+        for(auto it:s){
+            ans.push_back(it);
+        }
+
+        return ans;
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        return helper(n);
+    }
+};
