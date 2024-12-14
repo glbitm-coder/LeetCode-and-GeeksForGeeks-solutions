e888dad84dfacde1ccaf2559d58027ee081a2a0b
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2024-12-14 20:01:45 +0530

    Time: 36 ms (36.64%), Space: 102.9 MB (72.1%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,51 +1,32 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        stack<int> s;
-        int n = (int)temperatures.size();
-        unordered_map<int, int> mp;
-        for(int i = 0; i < n; ++i)
-        {
-            mp[temperatures[i]] = 0;
-        }
-        vector<int> ans(n, - 1);
-        int max = *max_element(temperatures.begin(), temperatures.end());
-        for(int i = 0; i < n; ++i)
-        {
-            if(temperatures[i] == max)
-            {
-                ans[i] = 0;
-                mp[temperatures[i]] = 0;
+        stack<pair<int,int>> st;
+        unordered_map<int,int> mp;
+        int n = temperatures.size();
+        for(int i = n - 1; i >= 0; --i){
+            if(st.empty()){
+                st.push({i, 0});
+            }
+            else{
+                int val = 0;
+                while(!st.empty() && temperatures[st.top().first] <= temperatures[i]){
+                    temperatures[st.top().first] = st.top().second;
+                    val += st.top().second;
+                    st.pop();
+                }
+                if(st.empty()){
+                    st.push({i,0});
+                }
+                else{
+                    st.push({i, val + 1});
+                }
             }
         }
-        ans[n - 1] = 0;
-        mp[temperatures[n-1]] = 0;
-        for(int i = n-1; i >=0 ; --i)
-        {
-            int count = 0;
-            while(!s.empty() && temperatures[i] >= s.top())
-            {
-                count += mp[s.top()];
-                s.pop();
-            }
-            if(s.empty())
-            {
-                count = 0;
-                mp[temperatures[i]] = 0;
-            }
-            else
-            {
-                count += 1;
-            }
-           
-            s.push(temperatures[i]);
-            if(ans[i] == 0)
-            {
-                continue;
-            }
-             mp[temperatures[i]] = count;
-             ans[i] = count;
+        while(!st.empty()){
+            temperatures[st.top().first] = st.top().second;
+            st.pop();
         }
-        return ans;
+        return temperatures;
     }
 };
