2560b8eff5a9657455bf46c1a1cc81953565b722
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2024-12-14 20:03:43 +0530

    Time: 35 ms (41.51%), Space: 103 MB (69.82%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         stack<pair<int,int>> st;
-        int n = temperatures.size();
-        for(int i = n - 1; i >= 0; --i){
+        int val = 0;
+        for(int i = temperatures.size() - 1; i >= 0; --i){
             if(st.empty()){
                 st.push({i, 0});
             }
             else{
-                int val = 0;
+                val = 0;
                 while(!st.empty() && temperatures[st.top().first] <= temperatures[i]){
                     temperatures[st.top().first] = st.top().second;
                     val += st.top().second;
