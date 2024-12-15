e2fd8e10c23b7fd7d2464894f33c6b9026efda4a
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2024-12-15 19:42:45 +0530

    Time: 0 ms (100%), Space: 13.3 MB (20.56%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,54 +1,21 @@
 class Solution {
 public:
-    bool binary(vector<vector<int>>& matrix, int a, int target)
-    {
-        int start = 0; 
-        int end = (int)matrix[0].size()-1;
-        int mid;
-        while(start <= end)
-        {
-            mid = (start + end) / 2;
-            if(target < matrix[a][mid])
-            {
-                end = mid - 1;
-            }
-            else if(target > matrix[a][mid])
-            {
-                start = mid + 1;
-            }
-            else if(target == matrix[a][mid])
-            {
-                return true;
-            }
-        }
-        return false;
-    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int start = 0;
-        int n = (int)matrix[0].size() - 1;
-        int end = (int)matrix.size() - 1;
-        int mid;
+        int n = matrix[0].size();
+        int m = matrix.size();
+        int start = 0, end = n * m - 1, mid = 0, num = 0;
+
         while(start <= end){
-            mid = (start + end) / 2;
-            if(target == matrix[mid][0])
-            {
+            mid = start + (end - start) / 2;
+            num = matrix[mid/n][mid%n];
+            if(num == target){
                 return true;
             }
-            if(target == matrix[mid][n])
-            {
-                return true;
-            }
-            if(target < matrix[mid][0])
-            {
-                end = mid - 1;
-            }
-            else if( target > matrix[mid][n])
-            {
+            else if(target > num){
                 start = mid + 1;
             }
-            else if(target > matrix[mid][0] && target < matrix[mid][n])
-            {
-                return binary(matrix, mid, target);
+            else {
+                end = mid - 1;
             }
         }
         return false;
