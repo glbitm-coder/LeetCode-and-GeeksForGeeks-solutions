6996584bfcac6ed0af19918726f8f5bf86985dab
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2024-12-15 21:12:27 +0530

    Time: 15 ms (26.75%), Space: 23.1 MB (9.75%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int end = *max_element(piles.begin(), piles.end());
-        int start = 1,mid = 0;
+        int start = 1,mid = 0, ans = end;
         while(start <= end){
             mid = start + (end - start) / 2;
             long long int minTime = 0;
@@ -13,11 +13,12 @@ public:
             }
             if(minTime <= h){
                 end = mid - 1;
+                ans = min(ans, mid);
             }
             else{
                 start = mid + 1;
             }
         }
-        return start;
+        return ans;
     }
 };
