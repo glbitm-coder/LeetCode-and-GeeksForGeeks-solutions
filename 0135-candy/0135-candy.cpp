7efda03f2e0f1a3432ad3552441eaa587d8bbc7a
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2024-12-20 00:09:18 +0530

    Time: 210 ms (5.03%), Space: 24.1 MB (5.04%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n = ratings.size(); // Get the number of children
+        // [3,2,1,2,3]
+        //   3 2 1 2 3
+
+        priority_queue <pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> p;
         
-        vector<int> candies(n, 1); // Initialize a vector to store the number of candies for each child
-        
-        // First pass: Check ratings from left to right
-        for(int i = 1; i < n; i++){
-            if(ratings[i - 1] < ratings[i] && candies[i - 1] >= candies[i]){
-                // If the current child has a higher rating and fewer or equal candies than the previous child,
-                // give them one more candy than the previous child
-                candies[i] = candies[i - 1] + 1;
+        int n = ratings.size();
+        vector<int> ans(n, 1);
+
+        for(int i = 0; i < n; ++i){
+            p.push({ratings[i],i});
+        }
+
+        while(!p.empty()){
+            pair<int,int> v = p.top();
+            int left = v.second - 1;
+            int right = v.second + 1;
+            if(left >= 0 && ratings[left] > v.first){
+                ans[left] = max(ans[left], ans[v.second] + 1);
             }
-        }
-        
-        // Second pass: Check ratings from right to left
-        for(int i = n - 2; i >= 0; i--){
-            if(ratings[i + 1] < ratings[i] && candies[i + 1] >= candies[i]){
-                // If the current child has a higher rating and fewer or equal candies than the next child,
-                // give them one more candy than the next child
-                candies[i] = candies[i + 1] + 1;
+            if(right < n &&  ratings[right] > v.first){
+                ans[right] = max(ans[right], ans[v.second] + 1);
             }
+            p.pop();
         }
-        
-        int totalCandies = 0;
-        
-        // Calculate the total number of candies needed
-        for(int i = 0; i < n; i++){
-            totalCandies += candies[i];
+        int res = 0;
+        for(int i = 0; i < n; ++i){
+            res += ans[i];
+            cout<<ans[i]<<" ";
         }
-        
-        return totalCandies;
+        return res;
     }
 };
