a03fb6f7452641dac07fcc58b6b388e82e7a57ac
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2024-12-22 17:09:25 +0530

    Time: 24 ms (26.91%), Space: 11.5 MB (21.95%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,54 +1,42 @@
 class Solution {
 public:
-    bool check(unordered_map<char,int> &mp1, unordered_map<char,int> &mp2)
-    {
-        if(mp1.size() != mp2.size())
-            return false;
-
-        for(auto it:mp2){
-            if(it.second < mp1[it.first]){
-                return false;
-            }
-        }
-        return true;
-    }
+    
     string minWindow(string s, string t) {
+
         unordered_map<char,int> mp, mp1;
         pair<int,int> ans;
+        
         ans.first = INT_MAX;
         ans.second = -1;
+        
         for(int i = 0; i < t.size(); ++i){
             mp[t[i]]++;
         }
 
+        int need = mp.size();
+        int have = 0;
+        
         for(int i = 0, j = 0; i < s.size(); ++i){
-
-            if(i - j + 1 > ans.first){
-                if(j < s.size() && t.find(s[j]) != string::npos){
-                    mp1[s[j]]--;
-                }
-                ++j;
-                while(j < s.size() && t.find(s[j]) == string::npos){
-                    ++j;  
-                }
-            }
 
             if(t.find(s[i]) != string::npos){
                 mp1[s[i]]++;
-                
-                while(check(mp,mp1)){
+                char k = s[i];
+                if(mp1[k] == mp[k]){
+                    ++have;
+                }
+                while(have >= need){
                     if(i - j + 1 < ans.first){
                         ans.first = i - j + 1;
                         ans.second = j;
                     }
                     if(j < s.size() && t.find(s[j]) != string::npos){
                         mp1[s[j]]--;
+                        if(mp1[s[j]] < mp[s[j]]){
+                            --have;
+                        }
+                        if(mp1[s[j]] == 0) mp1.erase(s[j]);
                     }
-                    
                     ++j;
-                    while(j < s.size() && t.find(s[j]) == string::npos){
-                        ++j;
-                    }
                 }
             }
             
