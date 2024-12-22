909724790e5e7dd836fe3832ed2c2d3f2c4cef45
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2024-12-23 01:53:11 +0530

    Time: 59 ms (27.25%), Space: 153.2 MB (16.93%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n = nums.size();
+
+        priority_queue<pair<int,int>> pq;
         vector<int> ans;
-        if(n <= k)
-        {
-            int p = *max_element(nums.begin(), nums.end());
-            ans.push_back(p);
+        if(k >= nums.size()){
+            ans.push_back(*max_element(nums.begin(), nums.end()));
             return ans;
         }
-        multiset<int, greater<int> > ms;
-        for(int i = 0; i < k; ++i)
-        {
-            ms.insert(nums[i]);
+        for(int i = 0; i < k; ++i){
+            pq.push({nums[i], i});
         }
-        ans.push_back(*(ms.begin()));
-        for(int i = 1, j = k; j < n; ++i, ++j)
-        {
-            ms.erase(ms.find(nums[i - 1]));
-            ms.insert(nums[j]);
-            ans.push_back(*(ms.begin()));
+        ans.push_back(pq.top().first);
+        for(int i = 1, j = k; j < nums.size(); ++i, ++j){
+            pq.push({nums[j], j});
+            while(pq.top().second < i){
+                pq.pop();
+            }
+            ans.push_back(pq.top().first);
         }
+           
         return ans;
     }
 };
