011bb815135f0a20090bdac0a0bef1462329719a
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2024-12-22 17:20:43 +0530

    Time: 16 ms (49.62%), Space: 11.8 MB (8.68%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -18,26 +18,23 @@ public:
         
         for(int i = 0, j = 0; i < s.size(); ++i){
 
-            if(t.find(s[i]) != string::npos){
-                mp1[s[i]]++;
-                char k = s[i];
-                if(mp1[k] == mp[k]){
-                    ++have;
+            mp1[s[i]]++;
+            char k = s[i];
+            if(mp1[k] == mp[k]){
+                ++have;
+            }
+            while(have >= need){
+                if(i - j + 1 < ans.first){
+                    ans.first = i - j + 1;
+                    ans.second = j;
                 }
-                while(have >= need){
-                    if(i - j + 1 < ans.first){
-                        ans.first = i - j + 1;
-                        ans.second = j;
+                mp1[s[j]]--;
+                if(j < s.size() && mp.find(s[j]) != mp.end()){
+                    if(mp1[s[j]] < mp[s[j]]){
+                        --have;
                     }
-                    if(j < s.size() && t.find(s[j]) != string::npos){
-                        mp1[s[j]]--;
-                        if(mp1[s[j]] < mp[s[j]]){
-                            --have;
-                        }
-                        if(mp1[s[j]] == 0) mp1.erase(s[j]);
-                    }
-                    ++j;
                 }
+                ++j;
             }
             
         }
