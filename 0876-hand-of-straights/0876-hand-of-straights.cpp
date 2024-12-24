f77fdd1d2e06c52b939eeafdc0c6b773da8a7e21
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2024-12-24 20:52:49 +0530

    Time: 815 ms (5.02%), Space: 33.3 MB (12.15%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.cpp
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -4,30 +4,28 @@ public:
         int n = hand.size();
         if(n % groupSize != 0) return false;
 
-        sort(hand.begin(), hand.end());
 
-        map<int,int> mp;
+        unordered_map<int,int> mp;
         for(int i = 0; i < n; i++){
             mp[hand[i]]++;
         }
-
-        int count = groupSize - 1, start = 0;
-        for(int i = 0; i < n / groupSize; ++i){
-            
-            start = mp.begin()->first;
-            count = groupSize - 1;
-            mp[start]--;
-            
-            if(mp[start] == 0) mp.erase(start);
-            
-            while(count--){
-                if(mp.find(start + 1) == mp.end()){
-                    
-                    return false;
-                }
-                else{
-                    mp[start + 1]--;
-                    if(mp[start + 1] == 0) mp.erase(start + 1);
+        int start = 0;
+        for(int i = 0; i < n; ++i){
+            start = hand[i];
+            while(mp.find(start - 1) != mp.end()){
+                start = start - 1;
+            }
+            while(start <= hand[i]){
+                while(mp[start] > 0){
+                    for(int j = start; j < start + groupSize; ++j){
+                        if(mp.find(j) == mp.end()) return false;
+                        else{
+                            --mp[j];
+                            if(mp[j] == 0){
+                                mp.erase(j);
+                            }
+                        }
+                    }
                 }
                 ++start;
             }
