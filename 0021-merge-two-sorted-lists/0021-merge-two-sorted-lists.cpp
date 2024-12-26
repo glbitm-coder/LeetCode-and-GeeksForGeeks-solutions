e24ff4a7f2c592fc24d79c0c5360008c7681d2d0
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2024-12-26 21:11:01 +0530

    Time: 0 ms (100%), Space: 19.5 MB (82.57%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,48 +11,35 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* head;
-        if(list1 == NULL)
-        {
-            return list2;
-        }
-        if(list2 == NULL)
-        {
-            return list1;
-        }
-        if(list1->val <= list2->val)
-        {
+        ListNode* head= NULL, *curr = NULL;
+        if(list1 == NULL) return list2;
+        if(list2 == NULL) return list1;
+        if(list1->val <= list2->val){
             head = list1;
             list1 = list1->next;
         }
-        else
-        {
+        else{
             head = list2;
             list2 = list2->next;
         }
-        ListNode* ans = head;
-        while(list1 != NULL && list2 != NULL)
-        {
-            if(list1->val <= list2->val)
-            {
-                head->next = list1;
-                list1 = list1 -> next;
+        curr = head;
+        while(list1 != NULL && list2 != NULL){
+            if(list1->val <= list2->val){
+                curr->next= list1;
+                list1 = list1->next;
             }
             else{
-                head->next = list2;
-                list2 = list2 -> next;
+                curr->next = list2;
+                list2 = list2->next;
             }
-            head = head->next;
-            
+            curr = curr->next;
         }
-        if(list1 != NULL)
-        {
-            head->next = list1;
+        if(list1 != NULL){
+            curr->next = list1;
         }
-        if(list2 != NULL)
-        {
-            head->next = list2;
+        else if(list2 != NULL){
+            curr->next = list2;
         }
-        return ans;
+        return head;
     }
 };
