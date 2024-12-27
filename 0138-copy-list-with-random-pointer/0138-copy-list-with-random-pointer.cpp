33118e912518bce1d3843709079881ff592b7aa8
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2024-12-27 15:35:38 +0530

    Time: 8 ms (28.59%), Space: 15.3 MB (9.04%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,32 +17,22 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if(not head) 
-            return head;
-// Modifying the input list:
-        Node *temp = head, *ptr = head;
-        while(ptr){
-            temp = ptr->next;
-            ptr->next = new Node(ptr->val);
-            ptr->next->next = temp;
-            ptr = temp;
+        if(head == NULL){
+            return NULL;
         }
-// List Updated to: [7]-[copy7]->[13]->[copy13]... 
-
-//  Updating the random pointers
-	ptr = head;
-    while(ptr){
-            ptr->next->random = (ptr->random)? ptr->random->next : nullptr;
-            ptr = ptr->next->next;
+        unordered_map<Node* , Node*> mp;
+        Node* curr = head;
+        while(curr != NULL){
+            mp[curr] = new Node(curr->val);
+            curr = curr->next;
         }
-//  Disconnecting to generate cloned list.
-        Node *copyHead = head->next, *copyptr = head->next;
-        while(head){
-            head->next = head->next->next;
-            copyptr->next = copyptr->next? copyptr->next->next : nullptr;
-            head = head->next;
-            copyptr = copyptr->next;
-        }   
-     return copyHead; 
+        curr = head;
+        while(curr != NULL){
+            mp[curr]->next = mp[curr->next];
+            mp[curr]->random = mp[curr->random];
+            curr = curr->next;
+        }
+        curr = mp[head];
+        return curr;
     }
 };
