cdca07475f807c648a4701b57638456ccb9dc2cd
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2025-01-10 02:30:22 +0530

    Time: 2 ms (62.46%), Space: 14.6 MB (40.79%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    vector<int> temp;
     void backtrack(int prev , vector<int> &candidates, int target, set<vector<int>> &ans, vector<int> &curr){
         
         if(target < 0) return;
         if(target == 0){
-            temp = curr;
-            sort(temp.begin(), temp.end());
-            ans.insert(temp);
-            temp.clear();
+            ans.insert(curr);
             return;
         }
         for(int i = prev; i < candidates.size(); ++i){
             curr.push_back(candidates[i]);
-            // target = target - candidates[i];
-            // cout<<target<<" "<<candidates[i]<<"\n"
             backtrack(i, candidates, target - candidates[i], ans, curr);
-            // target = target + candidates[i];
             curr.pop_back();
         }
         return;
