27c32fca02a6bb8ffc4873a8811f8a51c09290a0
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2025-01-10 02:36:08 +0530

    Time: 0 ms (100%), Space: 14.2 MB (52.66%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    void backtrack(int prev , vector<int> &candidates, int target, set<vector<int>> &ans, vector<int> &curr){
+     set<vector<int>> ans;
+    void backtrack(int prev , vector<int> &candidates, int target, vector<int> &curr){
         if(target < 0) return;
         if(target == 0){
             ans.insert(curr);
@@ -8,7 +9,7 @@ public:
         }
         for(int i = prev; i < candidates.size(); ++i){
             curr.push_back(candidates[i]);
-            backtrack(i, candidates, target - candidates[i], ans, curr);
+            backtrack(i, candidates, target - candidates[i], curr);
             curr.pop_back();
         }
         return;
@@ -16,9 +17,8 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         
         vector<int> curr;
-        set<vector<int>> s;
-        backtrack(0, candidates, target, s, curr);
-        vector<vector<int>> ans(s.begin(), s.end());
-        return ans;
+        backtrack(0, candidates, target,  curr);
+        vector<vector<int>> res(ans.begin(), ans.end());
+        return res;
     }
 };
