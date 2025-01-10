e6c14e49fcdfdab714bd9547ba19bd8b7ced3f48
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2025-01-10 16:19:42 +0530

    Time: 4 ms (14.07%), Space: 11.3 MB (24.16%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    void backtrack(unordered_set<int> &visited, vector<int> &nums, vector<vector<int>> &ans, vector<int> &curr){
+     vector<vector<int>> ans;
+    void backtrack(unordered_set<int> &visited, vector<int> &nums, vector<int> &curr){
         if(curr.size() == nums.size()){
             ans.push_back(curr);
             return;
@@ -9,16 +10,15 @@ public:
             if(visited.find(nums[i]) != visited.end()) continue;
             visited.insert(nums[i]);
             curr.push_back(nums[i]);
-            backtrack(visited, nums, ans, curr);
+            backtrack(visited, nums, curr);
             curr.pop_back();
             visited.erase(nums[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
         vector<int> curr;
         unordered_set<int> visited;
-        backtrack(visited, nums, ans, curr);
+        backtrack(visited, nums, curr);
         return ans;
     }
 };
