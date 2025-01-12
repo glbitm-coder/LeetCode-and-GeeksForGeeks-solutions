0e2d2b44e7e3bfac7341b7fefb4d802da0128081
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2025-01-12 15:08:15 +0530

    Time: 8 ms (6.19%), Space: 12.4 MB (9.06%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,53 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    void createNodes(Node* node, unordered_map<Node*, Node*> &mp, unordered_map<Node*, bool> &visited){
+        if(node == NULL) return;
+        if(visited[node]) return;
+        visited[node] = true;
+        Node* newNode = new Node(node->val);
+        mp[node] = newNode;
+        for(auto it: node->neighbors){
+            createNodes(it, mp, visited);
+        }
+    }
+
+    void connect(Node* node, unordered_map<Node*, Node*> &mp, unordered_map<Node*, bool> &visited){
+        if(node == NULL) return;
+        if(visited[node]) return;
+        visited[node] = true;
+        for(auto it:node->neighbors){
+            mp[node]->neighbors.push_back(mp[it]);
+            connect(it, mp, visited);
+        }
+    }
+    
+    Node* cloneGraph(Node* node) {
+        unordered_map<Node*, Node*> mp;
+        unordered_map<Node*, bool> visited;
+        createNodes(node, mp, visited);
+        visited.clear();
+        connect(node, mp, visited);
+        return mp[node];
+    }
+};
