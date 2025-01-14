f2bbeb34b848ab3462026f52565f04124ca8c56c
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2025-01-14 21:09:13 +0530

    Time: 0 ms (100%), Space: 14.3 MB (68.96%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,52 +1,36 @@
 class Solution {
 public:
-
-    void dfs(int x, int y, int rows, int cols, vector<vector<char>> &board, vector<vector<bool>> &visited)
+    vector<int> xDir{1, -1, 0, 0};
+    vector<int> yDir{0, 0, -1, 1};
+    void dfs(int x, int y, int n, int m, vector<vector<char>> &board, vector<vector<bool>> &visited)
     {
-        if(visited[x][y]){
-            return;
-        }
-
-        visited[x][y] = true;
-        board[x][y] = '1';
-
-        vector<int> x_val{0, 0, 1, -1};
-        vector<int> y_val{1, -1, 0, 0};
-
-        for(int i = 0; i < 4; ++i)
-        {
-            int xValue = x + x_val[i];
-            int yValue = y + y_val[i];
-
-            if(xValue >= 0 && yValue >= 0 && xValue < rows && yValue < cols)
-            {
-                if(board[xValue][yValue] == 'O' && !visited[xValue][yValue])
-                {
-                    dfs(xValue, yValue, rows, cols, board, visited);
-                }
-            } 
+        if(visited[x][y]) return;
+        board[x][y] = 'P';
+        for(int i = 0; i < 4; ++i){
+            int xVal = x + xDir[i];
+            int yVal = y + yDir[i];
+            if(xVal >= 0 && yVal >= 0 && xVal < n && yVal < m && !visited[xVal][yVal] && board[xVal][yVal] == 'O'){
+                dfs(xVal, yVal, n, m, board, visited);
+            }
         }
     }
+
     void solve(vector<vector<char>>& board) {
-        int rows = board.size();
-        int cols = board[0].size();
-
-        vector<vector<bool>> visited(rows, vector<bool> (cols, false));
-
-        for(int i = 0; i < rows; ++i){
-            for(int j = 0; j < cols; ++j){
-                if((i == 0 || j == 0 || i == rows - 1 || j == cols - 1) && (board[i][j] == 'O')){
-                    dfs(i, j, rows, cols, board, visited);
+        int n = board.size(), m = board[0].size();
+        vector<vector<bool>> visited(n, vector<bool> (m, 0));
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < m; ++j){
+                if((i == 0 || j == 0 || i == n - 1 || j == m - 1) && !visited[i][j] && board[i][j] == 'O'){
+                    dfs(i, j, n, m, board, visited);
                 }
             }
         }
-
-        for(int i = 0; i < rows; ++i){
-            for(int j = 0; j < cols; ++j){
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < m; ++j){
                 if(board[i][j] == 'O'){
                     board[i][j] = 'X';
                 }
-                if(board[i][j] == '1'){
+                if(board[i][j] == 'P'){
                     board[i][j] = 'O';
                 }
             }
