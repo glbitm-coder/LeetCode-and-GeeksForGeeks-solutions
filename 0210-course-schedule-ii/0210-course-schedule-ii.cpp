c9bbf39cf5039f657824077d8fd64b8558a74233
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2025-01-15 16:35:09 +0530

    Time: 14 ms (8.51%), Space: 19.8 MB (9.82%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,54 +1,37 @@
 class Solution {
 public:
-    bool isCyclic(int V, vector<vector<int>> &adj, vector<int> &ans) {
-        //bfs
-        //kahn's algo
-        vector<int> inDegree(V, 0);
-	    for(int i = 0; i < V; ++i){
-	        for(int j = 0; j < adj[i].size(); ++j){
-	            inDegree[adj[i][j]]++;
-	        }
-	    }
-	    
-	    queue<int> q;
-	    for(int i = 0; i < inDegree.size(); ++i){
-	        if(inDegree[i] == 0){
-	            q.push(i);
-	        }
-	    }
-	    
-	    int count = 0;
-	    while(!q.empty()){
-	        int curr = q.front();
-	        ++count;
-	        q.pop();
-            ans.push_back(curr);
-	        for(int i = 0; i < adj[curr].size(); ++i){
-	            if(inDegree[adj[curr][i]] == 0){
-	                continue;
-	            }
-	            inDegree[adj[curr][i]]--;
-	            if(inDegree[adj[curr][i]] == 0){
-	                q.push(adj[curr][i]);
-	            }
-	        }
-	    }
-	    if( count != V){
-            ans.resize(0);
-            return true;
-        }              
-        return false;
-    }
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses, vector<int> ());
-        vector<int> ans;
-        for(int i = 0; i < prerequisites.size(); ++i){
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
-        }
-        isCyclic(numCourses, adj, ans);
-            
-        
-        return ans;
+    void dfs(int curr, unordered_map<int,int> &inDegree, unordered_map<int, vector<int>> &mp, vector<int> &ans, vector<bool> &visited){
+        visited[curr] = true;
 
+        ans.push_back(curr);
+        
+        for(auto it:mp[curr]){
+            inDegree[it]--;
+            if(!visited[it] && inDegree[it] == 0){
+                dfs(it, inDegree, mp, ans, visited);
+            }
+        }
+    }
+
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int,int> inDegree;
+        unordered_map<int,vector<int>> mp;
+        vector<bool> visited(numCourses, false);
+        vector<int> ans;
+
+        for(int i = 0; i < numCourses; ++i){
+            inDegree[i] = 0;
+        }
+        for(auto it:prerequisites){
+            inDegree[it[0]]++;
+            mp[it[1]].push_back(it[0]);
+        }
+        for(int i = 0; i < numCourses; ++i){
+            if(inDegree[i] == 0 && !visited[i]){
+                dfs(i, inDegree, mp, ans, visited);
+            }
+        }
+        if(ans.size() != numCourses) ans.resize(0);
+        return ans;
     }
 };
