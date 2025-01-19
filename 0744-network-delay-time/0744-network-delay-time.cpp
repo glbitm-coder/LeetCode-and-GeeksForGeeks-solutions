ff0717b76f37f9eaed0421dadb2a79001f44fcd5
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2025-01-20 01:27:47 +0530

    Time: 214 ms (5.01%), Space: 43.9 MB (91.34%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    void dfs(int curr, unordered_map<int, vector<pair<int,int>>> &mp, vector<int> &minTime){
+    void dfs(int curr, vector<pair<int,int>> mp[], vector<int> &minTime){
         int temp = 0;
         for(auto it:mp[curr]){
-            temp = it.second + minTime[curr - 1];
-            if(temp <  minTime[it.first - 1]){
-                minTime[it.first - 1] = temp;
+            temp = it.second + minTime[curr];
+            if( temp <  minTime[it.first]){
+                minTime[it.first] = temp;
                 dfs(it.first, mp, minTime);
             }
         }
         return;
     }
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<int> minTime(n, INT_MAX);
-        minTime[k - 1] = 0;
-        unordered_map<int, vector<pair<int,int>>> mp;
+        vector<int> minTime(n + 1, INT_MAX);
+        minTime[k] = 0;
+        vector<pair<int,int>> mp[n+1];
         for(int i = 0; i < times.size(); ++i){
             mp[times[i][0]].push_back({times[i][1], times[i][2]});
         }
 
         dfs(k, mp, minTime);
-        int val = *max_element(minTime.begin(), minTime.end());
+        int val = 0;
+        for(int i = 1; i <= n; ++i){
+            val = max(val, minTime[i]);
+        }
         return val == INT_MAX ? -1 : val;
     }
 };
