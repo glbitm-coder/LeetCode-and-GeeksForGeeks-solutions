4c801161d957f4c50de3dfe1c31739f5cbbc4e4f
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2025-01-20 01:38:18 +0530

    Time: 86 ms (56.68%), Space: 43.9 MB (91.34%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    void dfs(int curr, vector<pair<int,int>> mp[], vector<int> &minTime){
-        int temp = 0;
-        for(auto it:mp[curr]){
-            temp = it.second + minTime[curr];
-            if( temp <  minTime[it.first]){
-                minTime[it.first] = temp;
-                dfs(it.first, mp, minTime);
-            }
-        }
-        return;
-    }
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<int> minTime(n + 1, INT_MAX);
         minTime[k] = 0;
         vector<pair<int,int>> mp[n+1];
+        vector<bool> visited(n + 1, false);
+        priority_queue<pair<int,int> , vector<pair<int,int>>, greater<pair<int,int>>> pq;
         for(int i = 0; i < times.size(); ++i){
             mp[times[i][0]].push_back({times[i][1], times[i][2]});
         }
 
-        dfs(k, mp, minTime);
+        pq.push({0,k});
+        pair<int,int> temp;
+        int u;
+        while(!pq.empty()){
+            temp = pq.top();
+            pq.pop();
+            u = temp.second;
+            for(auto it:mp[u]){
+                if(!visited[it.first] && minTime[it.first] > minTime[u] + it.second){
+                    minTime[it.first] = minTime[u] + it.second;
+                    pq.push({minTime[it.first], it.first});
+                }
+            }
+        }
         int val = 0;
         for(int i = 1; i <= n; ++i){
             val = max(val, minTime[i]);
