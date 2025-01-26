a0da10f596fd3db4bfba4c58cccf0ad945d99160
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2025-01-27 00:00:19 +0530

    Time: 27 ms (13.59%), Space: 18.9 MB (15.51%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool helper(int idx, string s, string curr, unordered_set<string> &wordDict, unordered_map<int, bool> &mp){
+        cout<<idx<<" "<<curr<<"\n";
+        if(idx == s.size()){
+            mp[idx] = true;
+            return true;
+        }
+        if(mp.find(idx) != mp.end()){
+            return mp[idx];
+        }
+        for(int i = idx; i <s.size(); ++i ){
+            curr = curr + s[i];
+            if(wordDict.find(curr) != wordDict.end()){
+                bool val = helper(i + 1, s, "", wordDict, mp);
+                if(val){
+                    mp[idx] = val;
+                    return val;
+                }
+            }
+        }
+        mp[idx] = false;
+        return false;
+    }
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> us;
+        unordered_map<int, bool> mp;
+        for(auto it:wordDict){
+            us.insert(it);
+        }
+        return helper(0, s, "", us, mp);
+    }
+};
