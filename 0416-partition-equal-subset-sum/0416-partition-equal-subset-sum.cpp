61e5313256ee1adde979eb5bc0a01328df87e3f1
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2025-01-27 01:39:51 +0530

    Time: 61 ms (87.39%), Space: 21.9 MB (56.09%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    bool isSubsetSum(vector<int> &arr, int sum){
-        // code here 
-        int n = arr.size();
-        vector<vector<int>> dp(n, vector<int> (sum + 1, 0));
+    bool targetSum(vector<int>& nums, int n, int target){
+        int dp[n][target + 1];
         for(int i = 0; i < n; ++i){
-            for(int j = 0; j <= sum; ++j){
-                if(arr[i] == j){
-                    dp[i][j] = 1;
+            for(int j = 0; j <= target; ++j){
+                if(i == 0){
+                    if(j == nums[i]) dp[i][j] = 1;
+                    else dp[i][j] = 0;
                 }
-                else if(i - 1 >= 0 && dp[i - 1][j] == 1){
-                    dp[i][j] = 1;
-                }
-                else if(i - 1 >= 0 && j - arr[i] >= 0 && dp[i - 1][j - arr[i]] == 1){
-                    dp[i][j] = 1;
+                if(j == 0) dp[i][j] = 1;
+                if(i != 0 && j != 0){
+                    if(j < nums[i]) dp[i][j] = dp[i - 1][j];
+                    else{
+                        dp[i][j] = dp[i - 1][j - nums[i]] || dp[i - 1][j];
+                    }
                 }
             }
         }
-        if(dp[n - 1][sum] == 1){
-            return true;
-        }
-        return false;
+        return dp[n - 1][target];
     }
+
     bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-        int sum = 0;
+        int n = nums.size(), sum = 0;
         for(int i = 0; i < n; ++i){
             sum += nums[i];
         }
-
-        if(sum % 2 != 0)
-            return false;
-
-        return isSubsetSum(nums, sum / 2);
+        if(sum % 2 != 0) return false;
+        return targetSum(nums, n, sum / 2);
     }
 };
