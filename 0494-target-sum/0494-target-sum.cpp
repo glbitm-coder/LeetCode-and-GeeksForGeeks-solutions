8d49f1086e33aef98d0e062b4a0777bb1db54f19
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2025-01-28 01:46:24 +0530

    Time: 2 ms (91.02%), Space: 11.8 MB (63.61%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,53 +1,35 @@
 class Solution {
-public:int perfectSum(vector<int> &arr, int n, int sum)
-	{
-        // Your code goes here
-        int mod = 1e9 + 7;
-        vector<vector<int>> dp(n, vector<int> (sum + 1, 0));
+public:
+    int targetSum(vector<int>& nums, int n, int target){
+        int dp[n + 1][target + 1];
         
-        for(int i = 0; i <= sum; ++i){
-            if(i == arr[0]){
-                dp[0][i] = 1;
+        for(int i = 0; i <= n; ++i){
+            for(int j = 0; j <= target; ++j){
+                if(i == 0) dp[i][j] = 0;
+                if(j == 0) dp[i][j] = 1;
+                if(i != 0){
+                    if(j < nums[i - 1]) dp[i][j] = dp[i - 1][j];
+                    else{
+                        dp[i][j] = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j];
+                    }
+                }
             }
         }
-        
-        for(int i = 1; i < n; ++i){
-            for(int j = 0; j <= sum; ++j){
-                if(arr[i] > j){
-                    dp[i][j] = dp[i - 1][j];
-                }
-                else if(arr[i] == j){
-                    dp[i][j] = ((dp[i - 1][j] % mod) + dp[i - 1][0] + 1) % mod;
-                }
-                else{
-                    dp[i][j] = ((dp[i - 1][j] % mod) + (dp[i - 1][j - arr[i]] % mod)) % mod;
-                }
-                // cout<<i<<" "<<j<<" "<<dp[i][j]<<"\n";
-            }
-        }
-        return dp[n - 1][sum];
-	}
-    
-    int countPartitions(int n, int d, vector<int>& arr) {
-        // Code here
-        int sum = 0;
-        for(int i = 0; i < n; ++i){
-            sum += arr[i];
-        }
-        if((sum - d) % 2 != 0 || sum - d < 0){
-            return 0;
-        }
-        int target = (sum - d) / 2;
-        
-        
-        int ans = perfectSum(arr, n, target);
-        if(sum - d == 0){
-            return 1 + ans;
-        }
-        return ans;
+        return dp[n][target];
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        return countPartitions(n, target, nums);
+        int sum = 0,n = nums.size();
+        for(int i = 0; i < n; ++i){
+            sum += nums[i];
+        }
+        //s1 + s2 = sum
+        //s1 - s2 = target
+        //2s1 = sum + target
+        //s1 = (sum + target) / 2;
+        if((sum + target) % 2 != 0 || (sum + target) < 0){
+            return 0;
+        }
+        target = (sum + target) / 2;
+        return targetSum(nums, n, target);
     }
 };
