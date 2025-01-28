92bff6d0cefda94dac12e2fd55087bda3924af21
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2025-01-28 21:39:27 +0530

    Time: 14 ms (24.1%), Space: 25.6 MB (16.63%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,37 +1,53 @@
 class Solution {
 public:
-    bool is_intervalLeave(string s1, string s2, string s3, int i, int j, vector<vector<int>> &dp){
-        if(i == s1.size() && j == s2.size() )
-            return true;
-        if(dp[i][j] == 1 || dp[i][j] == 0) 
-            return dp[i][j];
-        if(i < s1.size() && s1[i] == s3[i + j]){
-            bool isValid = is_intervalLeave(s1,s2,s3,i + 1, j, dp);
-            if(isValid){
-                dp[i][j] = 1;
-                return true;
-            }
-            else{
-                dp[i][j] = 0;
+    bool isInter(int i, int j, int k, string s1, string s2, string s3, vector<vector<int>> &dp){
+        // cout<<i<<" "<<j<<" "<<k<<"\n";
+        if(i == s1.size() && j == s2.size()) return true;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(i == s1.size()){
+            if(s2[j] == s3[k])
+            {
+                dp[i][j] = isInter(i, j + 1, k + 1, s1, s2, s3, dp);
+                return dp[i][j];
             }
         }
-        if(j < s2.size()  && s2[j] == s3[i + j]){
-            bool isValid = is_intervalLeave(s1, s2, s3, i, j + 1, dp);
-            if(isValid){
-                dp[i][j] = 1;
-                return true;
-            }
-            else{
-                dp[i][j] = 0;
+        if(j == s2.size()){
+            if(s1[i] == s3[k]){
+                dp[i][j] = isInter(i + 1, j, k + 1, s1, s2, s3, dp);
+                return dp[i][j];
             }
         }
-        dp[i][j] = 0;
+        if(s1[i] != s2[j]){
+            if(s1[i] == s3[k]){
+                dp[i][j] = isInter(i + 1, j, k + 1, s1, s2, s3, dp);
+                return dp[i][j];
+            }
+            else if(s2[j] == s3[k]){
+                dp[i][j] = isInter(i, j + 1, k + 1, s1, s2, s3, dp);
+                return dp[i][j];
+            }
+            else{
+                dp[i][j] = false;
+                return dp[i][j];
+            }
+        }
+        else{
+            if(s1[i] == s3[k]){
+                dp[i][j] = isInter(i, j + 1, k + 1, s1, s2, s3, dp) || isInter(i + 1, j, k + 1, s1, s2, s3, dp);
+                return dp[i][j];
+            }
+            else{
+                dp[i][j] = false;
+                return false;
+            }
+        }
+        dp[i][j] = false;
         return false;
     }
     bool isInterleave(string s1, string s2, string s3) {
+        vector<vector<int>> dp(s1.size()+1, vector<int> (s2.size() + 1, -1));
         if(s1.size() + s2.size() != s3.size()) return false;
-        vector<vector<int>> dp(s1.size() + 1, vector<int> (s2.size() + 1, -1));
-        return is_intervalLeave(s1, s2, s3, 0, 0, dp);
-        
+        return isInter(0, 0, 0, s1, s2, s3, dp);
     }
 };
