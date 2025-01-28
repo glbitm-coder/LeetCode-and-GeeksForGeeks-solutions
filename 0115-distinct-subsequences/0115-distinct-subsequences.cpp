d841cee864440fad3a4bfe336b12b32e03a174a6
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2025-01-29 01:15:16 +0530

    Time: 19 ms (81.01%), Space: 17.2 MB (78.41%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        int n1 = s.size();
-        int n2 = t.size();
-        vector<vector<long double>> dp(n1, vector<long double> (n2, 0));
-
-        for(int i = 0; i < n1; ++i){
-            dp[i][0] = i == 0 ? (s[i] == t[0] ? 1 : 0) : ( s[i] == t[0] ? dp[i - 1][0] + 1 : dp[i - 1][0]);
-        }
-
-        for(int i = 1; i < n1; ++i){
-            for(int j = 1; j < n2; ++j){
-                if(s[i] == t[j]){
-                    dp[i][j] = (long double)dp[i - 1][j] + (long double)dp[i - 1][j - 1];
+        int n1 = s.size(), n2 = t.size();
+        unsigned long long dp[n1 + 1][n2 + 1];
+        for(int i = 0; i <= n1; ++i){
+            for(int j = 0; j <= n2; ++j){
+                if(i == 0 || j == 0){
+                    if(i == 0){
+                        dp[i][j] = 0;
+                    }
+                    if(j == 0){
+                        dp[i][j] = 1;
+                    }
                 }
                 else{
-                    dp[i][j] = dp[i - 1][j];
+                    if(s[i - 1] == t[j - 1]){
+                        dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+                    }
+                    else{
+                        dp[i][j] = dp[i - 1][j];
+                    }
                 }
             }
         }
-
-        return dp[n1 - 1][n2 - 1];
+        return dp[n1][n2];
     }
 };
