d67ce2de4d507f7cc63d66a4b6e8e60dce9b4fdf
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2025-01-28 22:00:18 +0530

    Time: 10 ms (26.53%), Space: 25.4 MB (17.64%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -5,45 +5,9 @@ public:
         if(i == s1.size() && j == s2.size()) return true;
         if(dp[i][j] != -1) return dp[i][j];
 
-        if(i == s1.size()){
-            if(s2[j] == s3[k])
-            {
-                dp[i][j] = isInter(i, j + 1, k + 1, s1, s2, s3, dp);
-                return dp[i][j];
-            }
-        }
-        if(j == s2.size()){
-            if(s1[i] == s3[k]){
-                dp[i][j] = isInter(i + 1, j, k + 1, s1, s2, s3, dp);
-                return dp[i][j];
-            }
-        }
-        if(s1[i] != s2[j]){
-            if(s1[i] == s3[k]){
-                dp[i][j] = isInter(i + 1, j, k + 1, s1, s2, s3, dp);
-                return dp[i][j];
-            }
-            else if(s2[j] == s3[k]){
-                dp[i][j] = isInter(i, j + 1, k + 1, s1, s2, s3, dp);
-                return dp[i][j];
-            }
-            else{
-                dp[i][j] = false;
-                return dp[i][j];
-            }
-        }
-        else{
-            if(s1[i] == s3[k]){
-                dp[i][j] = isInter(i, j + 1, k + 1, s1, s2, s3, dp) || isInter(i + 1, j, k + 1, s1, s2, s3, dp);
-                return dp[i][j];
-            }
-            else{
-                dp[i][j] = false;
-                return false;
-            }
-        }
-        dp[i][j] = false;
-        return false;
+        dp[i][j] = i < s1.size() && s1[i] == s3[k] && isInter(i + 1, j, k + 1, s1, s2, s3, dp) || 
+                    j < s2.size() && s2[j] == s3[k] && isInter(i, j + 1, k + 1, s1, s2, s3, dp);
+        return dp[i][j];
     }
     bool isInterleave(string s1, string s2, string s3) {
         vector<vector<int>> dp(s1.size()+1, vector<int> (s2.size() + 1, -1));
