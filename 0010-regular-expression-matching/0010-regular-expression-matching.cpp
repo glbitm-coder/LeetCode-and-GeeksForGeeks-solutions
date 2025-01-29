3f90f4135960d9a8ab91ac9197b71241647c734f
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2025-01-30 02:12:09 +0530

    Time: 3 ms (50.64%), Space: 9.6 MB (23.21%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        vector<vector<int>> dp(s.length()+1,vector<int>(p.length(),-1));
-        return helper(s,p,0,0,dp);
-    }
-    
-    bool helper(string s, string p, int i, int j,vector<vector<int>> &dp)
-    {
-        if(j==p.length())
-            return i==s.length();
-        if(dp[i][j]>=0)
-            return dp[i][j];
-        bool first_match=(i<s.length() && (s[i]==p[j] || p[j]=='.' ));
-        bool ans=0;
-        if(j+1<p.length() && p[j+1]=='*')
+    vector<vector<int>> dp(s.length()+1,vector<int>(p.length()+1,0));
+        dp[s.length()][p.length()]=1;
+        
+        for(int i=s.length();i>=0;i--)
         {
-            ans= (helper(s,p,i,j+2,dp)|| (first_match && helper(s,p,i+1,j,dp) ));
+            for(int j=p.length()-1;j>=0;j--)
+            {
+                bool first_match=(i<s.length() && (p[j]==s[i]|| p[j]=='.'));
+                    if(j+1<p.length() && p[j+1]=='*')
+                    {
+                        dp[i][j]=dp[i][j+2] || (first_match && dp[i+1][j]);
+                    }
+                else
+                {
+                    dp[i][j]=first_match && dp[i+1][j+1];
+                }
+            }
         }
-        else
-        {
-            ans= (first_match && helper(s,p,i+1,j+1,dp));
-        }
-        dp[i][j]=ans;
-        return ans;
+        
+    return dp[0][0];
     }
 };
