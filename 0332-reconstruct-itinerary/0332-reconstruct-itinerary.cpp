626b0ba327336c8165062204a4678c6760540f28
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2025-01-30 17:52:13 +0530

    Time: 11 ms (34.1%), Space: 22.1 MB (20.61%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     vector<string> ans;
-    void dfs(string curr, unordered_map<string, vector<pair<string,bool>>> &mp){
+    void dfs(string curr, unordered_map<string, multiset<string>> &mp){
         if(mp.find(curr) == mp.end()){
             ans.push_back(curr);
             return;
         }
 
-        for(int i = 0; i < mp[curr].size(); ++i){
-            if(mp[curr][i].second == false){
-                mp[curr][i].second = true;
-                dfs(mp[curr][i].first, mp);
-            }
+        while(mp[curr].size() > 0){
+            auto next = *(mp[curr].begin());
+            mp[curr].erase(mp[curr].find(next));
+            dfs(next, mp);
         }
         ans.push_back(curr);
         return;
     }
 
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        unordered_map<string, vector<pair<string,bool>>> mp;
+        unordered_map<string, multiset<string>> mp;
         for(auto it:tickets){
-            mp[it[0]].push_back({it[1],false});
+            mp[it[0]].insert(it[1]);
         }
-        for(auto it:mp){
-            sort(mp[it.first].begin(), mp[it.first].end());
-        }
+        // for(auto it:mp){
+        //     sort(mp[it.first].rbegin(), mp[it.first].rend());
+        // }
         dfs("JFK", mp);
         reverse(ans.begin(), ans.end());
         return ans;
