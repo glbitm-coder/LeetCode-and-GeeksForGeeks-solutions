cc88b7cfa80375de99ceb21e92d5a179190cc4a1
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2025-01-31 21:11:02 +0530

    Time: 23 ms (21.68%), Space: 15.9 MB (20.5%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -2,40 +2,33 @@ class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
-        vector<vector<bool>> visit(n, vector<bool>(n, false));
-        int minH = grid[0][0], maxH = grid[0][0];
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < n; col++) {
-                maxH = max(maxH, grid[row][col]);
-                minH = min(minH, grid[row][col]);
+        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
+        vector<vector<bool>> visited(n, vector<bool> (n, false));
+        pq.push({{grid[0][0],0,0}});
+        int xDir[4] = {0, 0, 1, -1};
+        int yDir[4] = {1, -1, 0, 0};
+        vector<int> temp;
+        int r = 0,c = 0,val = 0, ans = 0, xVal = 0, yVal = 0;
+        while(!pq.empty()){
+            temp = pq.top();
+            pq.pop();
+            val = temp[0];
+            r = temp[1];
+            c = temp[2];
+            ans = max(ans, val);
+            if(visited[r][c]) continue;
+            visited[r][c] = true;
+            if(r == n - 1 && c == n - 1){
+                return ans;
+            }
+            for(int i = 0; i < 4; ++i){
+                xVal = r + xDir[i];
+                yVal = c + yDir[i];
+                if(xVal >= 0 && yVal >= 0 && xVal < n && yVal < n && !visited[xVal][yVal]){
+                    pq.push({grid[xVal][yVal], xVal, yVal});
+                }
             }
         }
-
-        for (int t = minH; t < maxH; t++) {
-            if (dfs(grid, visit, 0, 0, t)) {
-                return t;
-            }
-            for (int r = 0; r < n; r++) {
-                fill(visit[r].begin(), visit[r].end(), false);
-            }
-        }
-        return maxH;
-    }
-
-private:
-    bool dfs(vector<vector<int>>& grid, vector<vector<bool>>& visit, 
-                                        int r, int c, int t) {
-        if (r < 0 || c < 0 || r >= grid.size() || 
-            c >= grid.size() || visit[r][c] || grid[r][c] > t) {
-            return false;
-        }
-        if (r == grid.size() - 1 && c == grid.size() - 1) {
-            return true;
-        }
-        visit[r][c] = true;
-        return dfs(grid, visit, r + 1, c, t) || 
-               dfs(grid, visit, r - 1, c, t) || 
-               dfs(grid, visit, r, c + 1, t) || 
-               dfs(grid, visit, r, c - 1, t);
+        return ans;
     }
 };
