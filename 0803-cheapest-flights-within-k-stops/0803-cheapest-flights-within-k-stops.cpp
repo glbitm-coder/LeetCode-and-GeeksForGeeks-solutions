e1141393de1e0d30b6c1c8039d1c640ad3e8fcab
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2025-02-01 16:10:30 +0530

    Time: 3 ms (62.94%), Space: 17 MB (95.64%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,40 +1,23 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        //Dijkstra
-        unordered_map<int, vector<pair<int,int>>> mp;
-        vector<vector<int>> dist(n, vector<int> (k + 1, INT_MAX));
-        for(int i = 0; i < flights.size(); ++i){
-            mp[flights[i][0]].push_back({flights[i][1],flights[i][2]});
-        }
-        dist[src][0] = 0;
-        
-
-        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
-        pq.push({0,src, -1});
-        vector<int> temp;
-        int val = 0, s = 0, stops = 0, newStop = 0, newVal = 0;
-        while(!pq.empty()){
-            temp = pq.top();
-            pq.pop();
-            val = temp[0];
-            s = temp[1];
-            stops = temp[2];
-            // cout<<s<<" "<<stops<<" "<<val<<" "<<"\n";
-            if(s == dst) return val;
-            if(stops == k) continue;
-            
-           
-            for(auto it:mp[s]){
-                newStop = stops + 1;
-                newVal = val + it.second;
-                if(dist[it.first][newStop] > newVal){
-                    dist[it.first][newStop] = newVal;
-                    pq.push({newVal, it.first, newStop});
+        //Bellman ford
+        vector<int> prices(n, INT_MAX), temp(n, INT_MAX);
+        int s = 0, d = 0, val = 0;
+        prices[src] = 0;
+        for(int i = 0; i <= k; ++i){
+            temp = prices;
+            for(int j = 0; j < flights.size(); ++j){
+                s = flights[j][0];
+                d = flights[j][1];
+                val = flights[j][2];
+                if(prices[s] == INT_MAX) continue;
+                if(prices[s] + val < temp[d]){
+                    temp[d] = prices[s] + val;
                 }
             }
+            prices = temp;
         }
-        
-        return -1;
+        return prices[dst] == INT_MAX ? -1 : prices[dst];
     }
 };
