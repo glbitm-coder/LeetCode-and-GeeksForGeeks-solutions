45dd490095f1aa73c4d10d1486fb483a9df4e57b
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2025-02-20 20:41:51 +0530

    Time: 48 ms (40.95%), Space: 42.2 MB (38.84%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,83 +1,47 @@
-class DisjointSet
-{
-    public:
-    vector<int> parent, size;
-    DisjointSet(int n){
-        parent.resize(n + 1);
-        size.resize(n + 1);
-        for(int i = 0; i < n; ++i){
-            parent[i] = i;
-            size[i] = 1;
-        }
-    }
-    
-    int findUPar(int node){
-        if(node == parent[node]){
-            return node;
-        }
-        return parent[node] = findUPar(parent[node]);
-    }
-    void unionBySize(int u, int v){
-        int parentU = findUPar(u);
-        int parentV = findUPar(v);
-        
-        if(parentU == parentV)
-            return;
-            
-        if(size[parentU] < size[parentV]){
-            parent[parentU] = parentV;
-            size[parentV] += size[parentU];
-        }
-        else{
-            parent[parentV] = parentU;
-            size[parentU] += size[parentV];
-        }
-    }
-    
-};
-
 class Solution {
 public:
-    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        int n = accounts.size();
-        unordered_map<string, int> account_no;
-        DisjointSet ds(n);
-        for(int i = 0; i < accounts.size(); ++i){
-            for(int j = 1; j < accounts[i].size(); ++j){
-                string email = accounts[i][j];
-                if(account_no.find(email) == account_no.end()){
-                    account_no[email] = i;
-                } else{
-                    ds.unionBySize(i, account_no[email]);
+    unordered_map<string, vector<int>> mp;
+
+    void dfs(int curr, vector<vector<string>>& accounts, vector<bool> &visited, set<string> &us){
+        
+        visited[curr] = true;
+        for(int i = 1; i < accounts[curr].size(); ++i){
+            string str = accounts[curr][i];
+            us.insert(str);
+            for(auto it: mp[str]){
+                if(!visited[it]){
+                    dfs(it, accounts, visited, us);
                 }
             }
         }
+    }
 
-        vector<vector<string>> emailsByIndex(n);
-        for(auto it:account_no){
-            int no = it.second;
-            int index = ds.findUPar(no);
-            emailsByIndex[index].push_back(it.first);
-        }
-
-        vector<vector<string>> ans;
-
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        
+        int n = accounts.size();
         for(int i = 0; i < n; ++i){
-            if(emailsByIndex[i].size() == 0){
-                continue;
+            for(int j = 1; j < accounts[i].size(); ++j){
+                mp[accounts[i][j]].push_back(i);
             }
-            sort(emailsByIndex[i].begin(), emailsByIndex[i].end());
-            vector<string> temp;
-
-            temp.push_back(accounts[i][0]);
-            for(auto it: emailsByIndex[i]){
-                temp.push_back(it);
-            }
-            ans.push_back(temp);
         }
 
-        sort(ans.begin(), ans.end());
+        set<string> us;
+        vector<vector<string>> ans;
+        vector<string> s;
+        vector<bool> visited(n, false);
+        for(int i = 0; i < n; ++i){
+            if(!visited[i]){
+                dfs(i, accounts, visited, us);
+                s.push_back(accounts[i][0]);
+                for(auto it: us){
+                    s.push_back(it);
+                }
+                ans.push_back(s);
+                s.clear();
+                us.clear();
 
+            }
+        }
         return ans;
     }
 };
